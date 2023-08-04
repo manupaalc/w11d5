47c8c4b492c5faff60f159b1cd5c66636fa62edc
farivar <farivaramiri@gmail.com>
2023-08-04 16:45:34 -0700

    phase 1

--- practice-for-ch-react-thunk-pokedex-long-practice-main/src/components/PokemonBrowser.js
+++ practice-for-ch-react-thunk-pokedex-long-practice-main/src/components/PokemonBrowser.js
@@ -1,21 +1,30 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
 
 import PokemonDetail from './PokemonDetail';
 import CreatePokemonForm from './CreatePokemonForm';
 import Fab from './Fab';
+import { getPokemon } from '../store/pokemon'; // Import the powerful getPokemon thunk action creator here!
 
 const PokemonBrowser = () => {
   const { pokemonId } = useParams();
   const pokemon = useSelector(state => {
     return state.pokemon.list.map(pokemonId => state.pokemon[pokemonId]);
   });
+
+  // Let's utilize the useDispatch hook to dispatch the getPokemon action
+  const dispatch = useDispatch();
+
   const [showForm, setShowForm] = useState(false);
 
+  useEffect(() => {
+    // Dispatch the getPokemon action right here to fetch all those cool Pokémon!
+    dispatch(getPokemon());
+  }, []); // The empty array as the second argument makes sure this runs only once after the component mounts
 
   if (!pokemon) {
-    return null;
+    return <div>Loading...</div>;
   }
 
   return (
